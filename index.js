43a96a329ca70c329cc07f4d5dfaa848dfee67d0
AUTOSAFE <107992265+AUTOSAFE@users.noreply.github.com>
2022-07-06 12:02:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const round = rs.question(`[+] Select Number  : `);
 
 });
 
-  const auth = rs.question('Auth Token : ');
+  const auth = rs.question('Auth Token Apa kontol : ');
   console.log('');
 console.clear()
   while (true) {
@@ -52,7 +52,7 @@ console.clear()
     const result = await GoStumble(auth);
     if (!result) {
 
-      console.log(chalk.red(`\r[ ${moment().format('HH:mm:ss')} ] Maybe Auth Token Expired ?`));
+      console.log(chalk.red(`\r[ ${moment().format('HH:mm:ss')} ] Maybe Auth Token Masuk ?`));
 
     } else if (result.includes('User')) {
 
@@ -64,10 +64,10 @@ console.clear()
 
 console.log(chalk.green(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.green(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.cyan(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.green(`Crown : ${crown}`)}
 ${chalk.red(` Status : Succes✓`)}\n`));
-await sleep(1500);
+await sleep(1000;
 
     } else if (result == 'BANNED') {
-      console.log(chalk.bgRed(`Akun lu di banned kontol?`));
+      console.log(chalk.bgRed(`SELAMAT KAMU HARUS ULANG?`));
      break;
     }
   }
